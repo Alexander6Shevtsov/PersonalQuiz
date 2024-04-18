af4becd8098996b17bd6e8aa75ce28fb12e8980a
Alexander6Shevtsov <axrsvr@gmail.com>
2024-04-18 16:49:42 +0300

    Initial Commit

---
+++ PersonalQuiz/QuestionsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  QuestionsViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexander Shevtsov on 18.04.2024.
+//
+
+import UIKit
+
+final class QuestionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+

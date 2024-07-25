b850bd469ffefb8045ea8706324e2e00cbdb2be9
Alexander6Shevtsov <axrsvr@gmail.com>
2024-07-25 16:17:34 +0300

    Add for view

---
+++ PersonalQuiz/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Alexander Shevtsov on 23.04.2024.
+//
+
+import Foundation
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Alexander Shevtsov on 19.04.2024.
+//
+
+import UIKit
+
+final class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}

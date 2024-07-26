2b9c61a5236b26ce85f98dd09f9160cf52807299
Alexander6Shevtsov <axrsvr@gmail.com>
2024-07-26 14:37:34 +0300

    Fix

--- PersonalQuiz/Models/Question.swift
+++ PersonalQuiz/Models/Question.swift
@@ -5,4 +5,80 @@
 //  Created by Alexander Shevtsov on 23.04.2024.
 //
 
-import Foundation
+    // если типы данных важны то struct
+struct Question { // если пользователь не может ничего менять, то все let
+    let title: String
+    let type: ResponseType // этот тип мы создали сами
+    let answers: [Answer] // сами создали тип
+    
+    // MVP - данные, пока нет других
+   static func getQuestions() -> [Question] { //static делает метод не наследуемым
+        [
+            Question(
+                title: "Какую пищу вы предпочитаете?",
+                type: .single,
+                answers: [
+                    Answer(title: "Стэйк", animal: .dog),
+                    Answer(title: "Рыбы", animal: .cat),
+                    Answer(title: "Морковь", animal: .rabbit),
+                    Answer(title: "Кукуруза", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "Что вам нравится больше?",
+                type: .multiple,
+                answers: [
+                    Answer(title: "Плавать", animal: .dog),
+                    Answer(title: "Спать", animal: .cat),
+                    Answer(title: "Обьниматься", animal: .rabbit),
+                    Answer(title: "Есть", animal: .turtle)
+                ]
+            ),
+            Question(
+                title: "На машине катаетесь?",
+                type: .ranged,
+                answers: [
+                    Answer(title: "Ненавижу", animal: .cat),
+                    Answer(title: "Нервничаю", animal: .rabbit),
+                    Answer(title: "Не замечаю", animal: .turtle),
+                    Answer(title: "Обожаю", animal: .dog)
+                ]
+            )
+        ]
+    }
+}
+
+// создаем свой тип данных
+enum ResponseType { // если типы данных не важны то enum
+    case single
+    case multiple
+    case ranged
+}
+
+struct Answer {
+    let title: String
+    let animal: Animal
+}
+
+enum Animal: Character {
+    case dog = "🐶"
+    case cat = "🐱"
+    case rabbit = "🐰"
+    case turtle = "🐢"
+    
+    var definition: String { // перебираем перечисление
+        switch self { // self - сами себя
+        case .dog:
+            return "Вам нравится быть с друзьями."
+        case .cat:
+            return "Вы себе на уме"
+        case .rabbit:
+            return "Вам нравиться все мягкое"
+        case .turtle:
+            return "Ваша сила - мудрость"
+        }
+    }
+}
+// пример как это работает
+//let animal: Animal = .dog
+//animal.definition
--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 final class QuestionsViewController: UIViewController {
+// из-за static не нужно создавать экземпляр класса
+// обращаемся к модели и вызываем метод
+    private let questions = Question.getQuestions()
 
     override func viewDidLoad() {
         super.viewDidLoad()
